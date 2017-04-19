da4eb5707dc77537bc2859e8996fddf7b7c1a504
railsroger <railsroger@gmail.com>
2017-04-19 02:40:54 -0700

    Create Photo Model

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :rooms
+  resources :photos
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418122734) do
+ActiveRecord::Schema.define(version: 20170419093011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "room_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.index ["room_id"], name: "index_photos_on_room_id", using: :btree
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string   "home_type"
@@ -64,5 +75,6 @@ ActiveRecord::Schema.define(version: 20170418122734) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "photos", "rooms"
   add_foreign_key "rooms", "users"
 end
