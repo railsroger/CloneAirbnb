dfec418cf3bdf4b47ca4f4367f205142dc6073f4
railsroger <railsroger@gmail.com>
2017-04-19 22:24:18 -0700

    Create Photo View

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :room
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Paperclip.options[:command_path] = "/usr/bin/"
 module CloneAirbnb
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
