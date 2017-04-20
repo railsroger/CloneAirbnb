00f51b6d2bd14b06ad88086f1490407020e59d41
railsroger <railsroger@gmail.com>
2017-04-20 03:05:59 -0700

    Create Room Index View

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,13 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Paperclip.options[:command_path] = "/usr/bin/"
+
+
 module CloneAirbnb
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
   end
 end
