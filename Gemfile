ac566c96d0b40a17207dc49ca2f6a451df1cc689
railsroger <railsroger@gmail.com>
2017-04-21 04:34:23 -0700

    Add Google Map

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'omniauth', '~> 1.6', '>= 1.6.1'
 gem 'omniauth-facebook', '~> 4.0'
 gem 'omniauth-google-oauth2', '~> 0.4.1'
 gem "paperclip"
+gem 'geocoder', '~> 1.4', '>= 1.4.3'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419093011) do
+ActiveRecord::Schema.define(version: 20170421101249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20170419093011) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_rooms_on_user_id", using: :btree
   end
 
